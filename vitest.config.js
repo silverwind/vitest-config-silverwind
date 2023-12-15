0376379d3b1ada3166836da9dfdda1e989dc6e2a
silverwind <me@silverwind.io>
2023-12-15 16:28:57 +0100

    pass url

--- vitest.config.js
+++ vitest.config.js
@@ -2,5 +2,5 @@ import {defineConfig} from "vitest/dist/config.js";
 import {backendTest} from "./index.js";
 
 export default defineConfig({
-  test: backendTest(),
+  test: backendTest({url: import.meta.url}),
 });
