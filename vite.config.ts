0e084e1515a560be4be7d23061d66a35a9aadc25
silverwind <me@silverwind.io>
2025-07-01 13:36:38 +0200

    remove target

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,6 @@ export default defineConfig({
     outDir: fileURLToPath(new URL("dist", import.meta.url)),
     minify: false,
     sourcemap: false,
-    target: "modules",
     emptyOutDir: true,
     chunkSizeWarningLimit: Infinity,
     assetsInlineLimit: 0,
